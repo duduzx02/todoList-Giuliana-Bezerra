1cc852591a2fa6aa051d16f3f11ee0a919128c49
Eduardo Lima <duduzx02@gmail.com>
2024-01-22 16:02:47 -0300

    First commit

---
+++ src/main/java/br/com/Eduardo/desafiotodoList/DesafioTodoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.Eduardo.desafiotodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Eduardo/desafiotodoList/DesafioTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Eduardo.desafiotodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
